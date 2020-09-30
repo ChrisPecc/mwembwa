d0410959c61a82fbe40e52892f2d828001657973
ChrisPecc <pecceu.christophe@gmail.com>
2020-09-30 10:10:53 +0200

    add connection and starting leave count

--- src/server/functions/user-function.js
+++ src/server/functions/user-function.js
@@ -6,13 +6,10 @@ const leavesFunctions = require("../functions/leaves-functions");
 
 const saveData = async (req, res, hash, leaves) => {
     const userExists = await User.exists({username: req.body.username});
-    // .then(resp => resp)
-    // .catch(error => error);
     console.log(userExists);
 
     const emailExists = await User.exists({email: req.body.email});
-    // .then(resp => resp)
-    // .catch(error => error);
+
     console.log(emailExists);
 
     if (userExists) {
@@ -36,7 +33,7 @@ const saveData = async (req, res, hash, leaves) => {
         .then(() => res.status(201).json({message: "new user enregistré!"}))
         .catch(error => res.status(500).json({message: error}));
 
-    return "it should never get there but I can't commit without it";
+    return "Done";
 };
 
 const signUp = async (req, res) => {
