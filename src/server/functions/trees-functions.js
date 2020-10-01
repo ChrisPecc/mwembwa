94fbdd38b65b566fc3afee4ef94cdb59cfdee6aa
ChrisPecc <pecceu.christophe@gmail.com>
2020-10-01 13:14:31 +0200

    tree buy wip

--- src/server/functions/trees-functions.js
+++ src/server/functions/trees-functions.js
@@ -3,6 +3,14 @@ import User from "../model/users";
 
 const displayAllTrees = (req, res) => {
     Tree.find()
+        // .populate("comments.user")
+        .then(resp => res.status(200).json({message: resp}))
+        .catch(error => res.status(500).json({message: error}));
+};
+
+const displayOneTree = (req, res) => {
+    Tree.findOne({_id: req.params.id})
+        .populate("owner")
         .populate("comments.user")
         .then(resp => res.status(200).json({message: resp}))
         .catch(error => res.status(500).json({message: error}));
@@ -41,4 +49,55 @@ const addComment = async (req, res) => {
     return "done";
 };
 
-module.exports = {displayAllTrees, addComment};
+const buyOneTree = async (req, res) => {
+    const targetTree = await Tree.findOne({_id: req.params.id});
+    console.log(targetTree);
+    const targetUser = await User.findOne({_id: req.body.user_id});
+    console.log(targetUser);
+    const basicTreeValue = Math.ceil(
+        (targetTree.height * targetTree.circumf) / Math.PI,
+    );
+    console.log(basicTreeValue);
+    let treeValue = 0;
+
+    if (targetTree.is_locked === true) {
+        return res
+            .status(403)
+            .json({message: "This tree is locked, it can't be bought!"});
+    }
+
+    if (targetTree.owner === req.body.user_id) {
+        return res
+            .status(409)
+            .json({message: "This Tree is already yours, smartass!"});
+    } else if (targetTree.owner === null) {
+        console.log("free tree case");
+        treeValue = basicTreeValue;
+        console.log(treeValue);
+    } else {
+        console.log("owned by other case");
+        treeValue = basicTreeValue * 2;
+        console.log(treeValue);
+    }
+
+    if (targetUser.leaves_count < treeValue) {
+        return res
+            .status(403)
+            .json({message: "You don't have enought leaves to buy that tree"});
+    }
+
+    User.updateOne(
+        {_id: req.body.user_id},
+        {leaves_count: targetUser.leaves_count - treeValue},
+    )
+        .then(() => {
+            Tree.updateOne({_id: req.params.id}, {owner: req.body.user_id})
+                .then(() => res.status(200).json({message: "Tree bought"}))
+                .catch(error => res.status(500).json({message: error}));
+        })
+        .catch(error => res.status(500).json({message: error}));
+
+    return "done";
+};
+
+module.exports = {displayAllTrees, addComment, displayOneTree, buyOneTree};
--- src/server/routes/tree-routes.js
+++ src/server/routes/tree-routes.js
@@ -3,6 +3,9 @@ const treerouter = express.Router();
 const treesFunctions = require("../functions/trees-functions");
 
 treerouter.get("/all", treesFunctions.displayAllTrees);
+treerouter.get("/one/:id", treesFunctions.displayOneTree);
+
 treerouter.post("/comment/:id", treesFunctions.addComment);
+treerouter.post("/buy/:id", treesFunctions.buyOneTree);
 
 module.exports = treerouter;
