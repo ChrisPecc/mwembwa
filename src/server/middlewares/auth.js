b8c45353c91464f0e5d9a1b7e29fee8cb84d70c1
ChrisPecc <pecceu.christophe@gmail.com>
2020-10-14 17:17:38 +0200

    auth middleware corrections

--- src/server/middlewares/auth.js
+++ src/server/middlewares/auth.js
@@ -1,19 +1,21 @@
+import {verify} from "jsonwebtoken";
 
-import { verify } from 'jsonwebtoken';
-
-const authMiddleware= (req, res, next) => {
+const authMiddleware = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = verify(token, "D4CC737A277F6B29C32D302E385C238AD8B2E1CC0C368B9FAD7537F9D27DD106");
+        const token = req.headers.authorization.split(" ")[1];
+        const decodedToken = verify(
+            token,
+            "D4CC737A277F6B29C32D302E385C238AD8B2E1CC0C368B9FAD7537F9D27DD106",
+        );
         const userId = decodedToken.user_id;
         if (req.body.user_id && req.body.user_id !== userId) {
-            throw 'Invalid user ID';    
-        } else {
-            next();    
+            return res.status(500).json({message: "user doesn't match"});
         }
+        next();
     } catch {
-        res.status(401).json({ message :"Invalid request"});
+        res.status(401).json({message: "Invalid request"});
     }
+    return "done";
 };
 
-module.exports = {authMiddleware}
+module.exports = {authMiddleware};
